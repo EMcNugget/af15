9a8cd1b537af58cff72143562d34656ea8ae173b
mrmcnuggetboi <ethan.d.seys@gmail.com>
2023-01-03 09:35:32 -0600

    new file:   .gitignore 	new file:   README.md 	new file:   __init__.py 	new file:   ui.py 	new file:   usafunitlib.py

---
+++ ui.py
@@ -0,0 +1,62 @@
+af = dict( # Reserved for future expansion
+
+)
+
+af15w = dict(             # Wings of the 15th Air Force
+    fw1 = "1st Fighter Wing",
+    fw4 = "4th Fighter Wing",
+    fw20 = "20th Fighter Wing",
+    w23 = "23d Wing",
+    agow93 = "93d Air-Ground Operations Wing",
+    fw325 = "325th Fighter Wing",
+    fw355 = "355th Fighter Wing",
+    fw366 = "366th Fighter Wing",
+    fw388 = "388th Fighter Wing",
+    w432 = "432nd Wing",
+    acw461 = "461st Air Control Wing",
+    fg495 = "495th Fighter Group",
+    acw552 = "552nd Air Control Wing",
+    abw633 = "633d Air Base Wing",
+)
+
+st = dict(                                  # Various terms for squadrons under a group
+    oss = "Operational Support Squadron",
+    fs = "Fighter Squadron",
+    fts = "Flying Training Squadron",
+    ts =  "Training Squadron",
+    ces = "Civil Engineer Squadron",
+    cs = "Communications Squadron",
+    cts = "Contracting Squadron",
+    fss = "Force Support Squadron",
+    lrs = "Logistics Readiness Squadron",
+    sfs = "Security Forces Squadron",
+    mt = "Maintenance Squadron",
+    mu = "Munitions Squadron",
+    ams = "Aerospace Medicine Squadron",
+    amxs = "Aircraft Maintenance Squadron",
+    ems = "Equipment Maintenance Squadron",
+    cms = "Component Maintenance Squadron",
+    fgs = "Fighter Generation Squadron",
+    omrs = "Operational Medical Readiness Squadron",
+    ds = "Dental Squadron",
+    mss = "Medical Support Squadron",
+    hos = "Healthcare Operations Squadron",
+    ss = "Surgical Squadron",
+    aacs = "Airborne Air Control Squadron",
+    acns = "Air Control Networks Squadron",
+    acs = "Air Control Squadron",
+    uavas = "Attack Squadron",
+    rs = "Reconnaissance Squadron",
+)
+
+gt = dict(                           # Various group terms under a wing
+    og = "Operations Group",
+    mg = "Maintenance Group",
+    meg = "Medical Group",
+    msg = "Mission Support Group",
+    fg = "Fighter Group",
+    rg = "Rescue Group",
+    bds = "Base-Defense Group",
+    asog = "Air Support Operations Group"
+)
+
---
+++ usafunitlib.py
@@ -0,0 +1,96 @@
+import ui
+def c2main():
+    class group:
+        @staticmethod
+        def opGroup():
+            return f"{x} {ui.gt['og']}"   
+        @staticmethod
+        def mgGroup():
+            return f"{x} {ui.gt['mg']}"
+        @staticmethod 
+        def megGroup():
+            return f"{x} {ui.gt['meg']}"
+        @staticmethod
+        def msgGroup():
+            return f"{x} {ui.gt['msg']}"
+        @staticmethod
+        def fgGroup():
+            return f"{x} {ui.gt['fg']}"
+        @staticmethod 
+        def rgGroup():
+            return f"{x} {ui.gt['rg']}"
+        @staticmethod
+        def bdsGroup():
+            return f"{x} {ui.gt['bds']}"
+        @staticmethod
+        def asogGroup():
+            return f"{x} {ui.gt['asog']}"
+
+    userInput = input("Select your wing under the 15th Air Force.")
+    if userInput == ui.af15w["fw1"]:
+        wingdata = ui.af15w["fw1"]
+        x = "1st"
+        myfunc = fw1g()
+    elif userInput == ui.af15w["fw4"]:
+        wingdata = ui.af15w["fw4"]
+        x = "4th"
+    elif userInput == ui.af15w["fw20"]:
+        wingdata = ui.af15w["fw20"]
+        x = "20th"
+    elif userInput == ui.af15w["w23"]:
+        wingdata = ui.af15w["w23"]
+        x = "23d"
+    elif userInput == ui.af15w["agow93"]:
+        wingdata = ui.af15w["agow93"]
+        x = "93d"
+    elif userInput == ui.af15w["fw325"]:
+        wingdata = ui.af15w["fw325"]
+        x = "325th"
+    elif userInput == ui.af15w["fw366"]:
+        wingdata = ui.af15w["fw366"]
+        x = "366th"
+    elif userInput == ui.af15w["fw388"]:
+        wingdata = ui.af15w["fw388"]
+        x = "388th"
+    elif userInput == ui.af15w["w432"]:
+        wingdata = ui.af15w["w432"]
+        x = "432nd" 
+    elif userInput == ui.af15w["acw461"]:
+        wingdata = ui.af15w["acw461"]
+        x = "461st"
+    elif userInput == ui.af15w["fg495"]:
+        wingdata = ui.af15w["fg495"]
+        x = "495th"
+    elif userInput == ui.af15w["acw552"]:
+        wingdata = ui.af15w["acw552"]
+        x = "552nd"
+    elif userInput == ui.af15w["abw633"]:
+        wingdata = ui.af15w["abw633"]
+        x = "633d"
+    else: 
+        print("No wing under the 15th Air Force exists under that title.")
+        return c2main()     
+
+    def return_():
+        y = "yes"
+        z = "Yes"
+        if userInput == y or z:
+            return myfunc
+
+    def fw1g():
+        if wingdata == ui.af15w["fw1"]:
+            print(f"The subordinate groups of the {wingdata} are the {group.opGroup()} and the {group.mgGroup()}")
+            userInput = input(f"Select a group under the {wingdata}")
+            if userInput == ' '.join([x, ui.gt['og']]):
+                print(f"The subordinate squadrons of the {x} {ui.gt['og']} are the {x} {ui.st['oss']}, 27th {ui.st['fs']}, 94th {ui.st['fs']}, 7th {ui.st['fts']}, and the 71st {ui.st['fts']}.")
+                if userInput == input("would you like to return?"):
+                    return_()
+            elif userInput == ' '.join([x,ui.gt['mg']]):
+                print(f"The subordinate squadrons of the {x} {ui.gt['mg']} are the {x} {ui.st['mt']} and the {x} {ui.st['mu']}.")
+                if userInput == input("would you like to return?"):
+                    return_()
+            else:
+                print(f"No group under that title exist under the {ui.af15w['fw1']}.")
+                return fw1g()
+    fw1g()         
+c2main()
