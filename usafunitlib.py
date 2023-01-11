db62dbd4c699ddd7d64a9aba135bb9c7dc545480
mrmcnuggetboi <ethan.d.seys@gmail.com>
2023-01-10 22:20:56 -0600

    GUI First major update:
    -Added support for images
    -Added patches for 15th AF, 1st FW, OG, and MXG
    -Added custom icon to application
    -Bar for images at the squadron level the GUI for the 1st FW is completed.
    -Redid formatting, and looks for descriptions
    -Refactored descriptions to save on character length
    -Added 15th Air Force description
    -Updated logging logic to create new file everytime the program runs
    -Various small formatting, hotfixes, and file system changes

--- usafunitlib.py
+++ usafunitlib.py
@@ -1,5 +1,6 @@
 import unitterms as ui # For wing, group, and squadron global data
 import logging as lg # For logging
+import logging.handlers
 import wingdb as wdb # Wing misc data
 import customtkinter # For GUI
 import tkinter # GUI dependency 
@@ -7,6 +8,8 @@ from PIL import Image, ImageTk # Image importing
 import os 
 
 lg.basicConfig(level=lg.DEBUG, handlers=[lg.FileHandler("unitliblog.txt")], format="%(asctime)s [%(levelname)s] %(message)s") # logging
+should_roll_over = os.path.isfile("unitliblog.txt")
+handler = logging.handlers.RotatingFileHandler("unitliblog.txt", mode='w', backupCount=5)
 
 class App(customtkinter.CTk): # GUI Framework 
     def __init__(self):
@@ -42,17 +45,19 @@ class App(customtkinter.CTk): # GUI Framework
         self.optionmenu.grid(row=3, column=0, padx=20, pady=(20, 10))
 
         # Image structure // reserved for later dev
-        self.image_frame = customtkinter.CTkFrame(self, width=200, height=240, corner_radius=10)
+        self.image_frame = customtkinter.CTkFrame(self,corner_radius=10)
         self.image_frame.grid(column=3, row=0, rowspan = 3, columnspan=1, padx=(20, 20), pady=(20, 20), sticky='nsew')
         self.image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets\\img_assets\\")
-        self.wing_1st_image = customtkinter.CTkImage(Image.open(os.path.join(self.image_path, "1stFW_display.png")).resize((200, 240),Image.Resampling.BILINEAR))
-        self.image_display = customtkinter.CTkLabel(master=self.image_frame, image=self.wing_1st_image, text="", fg_color=None, compound='center')
-        self.image_display.grid(column=3, row=0, rowspan = 3, columnspan=1)
+        self.af15 = customtkinter.CTkImage(Image.open(os.path.join(self.image_path, "af15.png")), size=(285, 280))
+        self.image_display = customtkinter.CTkLabel(master=self, image=self.af15, text="")
+        self.image_display.grid(column=3, row=0, rowspan = 3, columnspan=1, sticky='nsew')
+        
         # Description
-        self.textbox = customtkinter.CTkTextbox(self)
+        self.textbox = customtkinter.CTkTextbox(self, font=('Arial', 15), wrap='word')
         self.textbox.grid(row=0, column=1, columnspan=2, rowspan=4, padx=(20, 2.5), pady=(20, 20), sticky="nsew")
         self.textbox2 = customtkinter.CTkTextbox(self, corner_radius=10)
         self.textbox2.grid(row=3, rowspan=1, column=3, columnspan=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.textbox.insert("0.0", "15th Air Force\n\n" + ui.af15['af15desc'])
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
             customtkinter.set_appearance_mode(new_appearance_mode)
@@ -64,11 +69,24 @@ class App(customtkinter.CTk): # GUI Framework
                                                         variable=optionmenu_var)
         self.optionmenu.grid(row=3, column=0, padx=20, pady=(20, 10))
         self.textbox.delete('1.0', "end")
+        self.textbox.insert("0.0", "15th Air Force\n\n" + ui.af15['af15desc'])
+        self.af15 = customtkinter.CTkImage(Image.open(os.path.join(app.image_path, "af15.png")), size=(285, 280))
+        self.image_display = customtkinter.CTkLabel(master=self, image=self.af15, text="")
+        self.image_display.grid(column=3, row=0, rowspan = 3, columnspan=1, sticky='nsew')
 
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
-
+        
+    def image_load(patch):
+            App.patch = customtkinter.CTkImage(Image.open(os.path.join(app.image_path, f"{patch}.png")), size=(285, 280))
+            app.image_display = customtkinter.CTkLabel(master=app, image=app.patch, text="")
+            app.image_display.grid(column=3, row=0, rowspan = 3, columnspan=1, sticky='nsew')
+            
+    def insert_desc(unit, maindict, sub2dict, sub3dict):
+        app.textbox.delete('1.0', "end")
+        app.textbox.insert("0.0", f"{unit}\n\n" + maindict[sub2dict][sub3dict])
+    
     def c2main(wdata):
         class group: # Group Methods
             @staticmethod
@@ -119,68 +137,74 @@ class App(customtkinter.CTk): # GUI Framework
                     return f"{wt} {ui.gt['asog']}"
                 else: 
                     return f"{uid} {ui.gt['asog']}"
-                
+        
         if wdata == ui.af15w['fw1']: #1st Fighter Wing
             wt = wdb.db1.term
-            app.textbox.insert("0.0", ui.fw1d['wing'])
+            app.textbox.delete('1.0', "end")
+            app.textbox.insert("0.0", f"{ui.af15w['fw1']}\n\n" + ui.fw1d['wing'])
+            App.image_load('fw1st')
             def fw1g(x): # 1st Fighter Wing Units
                 lg.info(f"{wdb.cg(wdata)} {group.opGroup(x)} and the {group.mgGroup(x)}")
                 optionmenu_var1 = customtkinter.StringVar(value="Select a Group")
                 optionmenu_var2 = customtkinter.StringVar(value="Select a Squadron")
                 def fw1gui(wdata):
                     if wdata == ' '.join([wt, ui.gt['og']]):
-                        app.textbox.delete('1.0', "end")
-                        app.textbox.insert("0.0", ui.fw1d['opgroup']['opsg'])
+                        App.insert_desc(f"{wt} {ui.gt['og']}", ui.fw1d, 'opgroup', 'opsg')
+                        App.image_load('opg1st')
                         lg.info("1stOpGroup options and assets displayed")
                         def og1sq(wdata):
                             if wdata == f"{wt} {ui.st['oss']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['opgroup']['oss1'])
+                                App.insert_desc(f"{wt} {ui.st['oss']}", ui.fw1d, 'opgroup', 'oss1')
+                                App.image_load('blank')
                                 lg.info("1st OSS displayed")
                             elif wdata == f"27th {ui.st['fs']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['opgroup']['fs27'])
+                                App.insert_desc(f"27th {ui.st['fs']}", ui.fw1d, 'opgroup', 'fs27')
+                                App.image_load('blank')
                                 lg.info("27th FS displayed")
                             elif wdata == f"94th {ui.st['fs']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['opgroup']['fs94'])
+                                App.insert_desc(f"94th {ui.st['fs']}", ui.fw1d, 'opgroup', 'fs94')
+                                App.image_load('blank')
                                 lg.info("94th FS displayed")
                             elif wdata == f"7th {ui.st['ts']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['opgroup']['ts7'])
+                                App.insert_desc(f"7th {ui.st['ts']}", ui.fw1d, 'opgroup', 'ts7')
+                                App.image_load('blank')
                                 lg.info("7th TS displayed")
                             elif wdata == f"71st {ui.st['fts']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['opgroup']['fts71'])
+                                App.insert_desc(f"71st {ui.st['fts']}", ui.fw1d, 'opgroup', 'fts71')
+                                App.image_load('blank')
                                 lg.info("71st FTS displayed")
                             elif wdata == 'Return':
                                 app.textbox.delete('1.0', "end")
+                                App.image_load('fw1st')
+                                app.textbox.insert("0.0", f"{ui.af15w['fw1']}\n\n" + ui.fw1d['wing'])
                                 lg.info("Returning")
                                 return fw1g(x)
                         app.optionmenu.configure(values=["Select a Squadron...", f"{wt} {ui.st['oss']}", f"27th {ui.st['fs']}", f"94th {ui.st['fs']}", f"7th {ui.st['ts']}", f"71st {ui.st['fts']}", 'Return'], variable=optionmenu_var2, command=og1sq)
                     elif wdata == ' '.join([wt,ui.gt['mg']]):
-                        app.textbox.delete('1.0', "end")
-                        app.textbox.insert("0.0", ui.fw1d['maintgroup']['maintg'])
+                        App.insert_desc(f"{wt} {ui.gt['mg']}" ,ui.fw1d, 'maintgroup', 'maintg')
+                        App.image_load('mxg1st')
                         lg.info("1stMaintGroup units and assets displayed")
                         def mg1sq(wdata):
                             if wdata == f"{wt} {ui.st['mt']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['maintgroup']['mxs'])
+                                App.insert_desc(f"{wt} {ui.st['mt']}", ui.fw1d, 'maintgroup', 'mxs1')
+                                App.image_load('blank')
                                 lg.info("1st MXS displayed")
                             elif wdata == f"{wt} {ui.st['mu']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['maintgroup']['mu'])
+                                App.insert_desc(f"{wt} {ui.st['mu']}", ui.fw1d, 'maintgroup', 'mu1')
+                                App.image_load('blank')
                                 lg.info("1st MS displayed")
                             elif wdata == f"27th {ui.st['fgs']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['maintgroup']['fgs27'])
+                                App.insert_desc(f"27th {ui.st['fgs']}", ui.fw1d, 'maintgroup', 'fgs27')
+                                App.image_load('blank')
                                 lg.info("27th FGS displayed")
                             elif wdata == f"94th {ui.st['fgs']}":
-                                app.textbox.delete('1.0', "end")
-                                app.textbox.insert("0.0", ui.fw1d['maintgroup']['fgs94'])
+                                App.insert_desc(f"94th {ui.st['fgs']}", ui.fw1d, 'maintgroup', 'fgs94')
+                                App.image_load('blank')
                                 lg.info("94th FGS displayed")
                             elif wdata == 'Return':
                                 app.textbox.delete('1.0', "end")
+                                App.image_load('fw1st')
+                                app.textbox.insert("0.0", f"{ui.af15w['fw1']}\n\n" + ui.fw1d['wing'])
                                 lg.info("Returning")
                                 return fw1g(x)
                         app.optionmenu.configure(values=["Select a Squadron...", f"{wt} {ui.st['mt']}", f"{wt} {ui.st['mu']}", f"27th {ui.st['fgs']}", f"94th {ui.st['fgs']}", 'Return'], variable=optionmenu_var2, command=mg1sq)  
@@ -236,4 +260,5 @@ class App(customtkinter.CTk): # GUI Framework
             w23g(wt)
 if __name__ == "__main__":
     app = App()
+    app.iconbitmap("af15icon.ico")
     app.mainloop()
